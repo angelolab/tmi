2d79766c61c8b7880457b5108adfaa2771350bae
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-03-03 16:35:53 -0800

    added setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,40 @@
-# TODO: setup information to create a python package
+import setuptools
+from os import path, pardir
+from distutils.command.build_ext import build_ext as DistUtilsBuildExt
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+PKG_FOLDER = path.abspath(path.join(__file__, pardir))
+
+with open(path.join(PKG_FOLDER, 'requirements.txt')) as req_file:
+    requirements = req_file.read().splitlines()
+
+# set a long description which is basically the README
+with open(path.join(PKG_FOLDER, 'README.md')) as f:
+    long_description = f.read()
+
+
+setup(
+    name='tmi',
+    version=VERSION,
+    packages=find_packages(),
+    license='Modified Apache License 2.0',
+    description='Scripts and little tools which are used throughout ark-anaylsis, mibi-bin-tools, and toffy',
+    author='Angelo Lab',
+    url='https://github.com/angelolab/tmi',
+    download_url='https://github.com/angelolab/tmi/archive/v{}.tar.gz'.format(VERSION),
+    install_requires=requirements,
+    extras_require={
+        'tests': ['pytest',
+                  'pytest-cov',
+                  'pytest-pycodestyle',
+                  'testbook']
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['License :: OSI Approved :: Apache Software License',
+                 'Development Status :: 1 - Alpha',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.8']
+)
